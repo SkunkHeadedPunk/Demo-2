d647174b1c9c5e4000b6740b354c3c32de0addd5
SkunkHeadedPunk <jack.woolery.94@gmail.com>
2021-04-12 21:37:25 -0600

    add support and cam matrices for various resolutions (3264x2464 and 640x480)

--- ComputerVision_Vid.py
+++ ComputerVision_Vid.py
@@ -62,17 +62,6 @@ import smbus
 import busio
 import board
 
-### I2C INITIALIZATION
-##bus = smbus.SMBus(1)
-##i2c = busio.I2C(board.SCL, board.SDA)
-##
-### LCD INITIALIZATION
-##lcd_columns = 16
-##lcd_rows = 2
-##lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows)
-##lcd.clear()
-##lcd.color = [100, 0, 100]
-
 # FOR ZERO ANGLE CALIBRATION
 USE_CALIB_ANGLE = False
 
@@ -81,31 +70,26 @@ CALIB_ANGLE = - CALIB_ANGLE_FILE['zero_angle']
 
 
 # TO DISPLAY STREAM IMAGES
-DISP_STREAM = True
+DISP_STREAM = False
 # TO DISPLAY PRECISE IMAGE
 DISP_IMG = True
-# TO TIME STREAM FRAMES
-# THIS IS BROKEN, DON'T USE IT RIGHT NOW
-TIMING = False
 
+STATE1_WAITKEY = 1000
 # AMOUNT OF TIME TO DISPLAY STREAM IMAGES
 WAIT_KEY = 1
 # Set wait key to minimum for fastest stream when not displaying
 if DISP_STREAM == False:
     WAIT_KEY = 1
 
-## __________IMAGE SCALING__________ ##
-# Image scale for rapid detection
-SMALL_SCALE = 0.2
-# 0.25 -> 1.17 fps
+# Image capture dimensions
+# Max res: 3280 x 2464
+WIDTH, HEIGHT = 640, 480
+##WIDTH, HEIGHT = 3264, 2464
 
-# Image scale for precise detection
-LARGE_SCALE = 0.2
+width, height = str(WIDTH), str(HEIGHT)
+
 # Scale for resizing images for display
-DISP_SCALE = 0.5
-
-### Initialize camera
-##camera = PiCamera()
+DISP_SCALE = 1
 
 # Get the Aruco dictionary
 arucoDict = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_4X4_100)
@@ -113,15 +97,9 @@ arucoDict = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_4X4_100)
 # Measured Aruco marker length in inches
 MARKER_LENGTH_IN = 3.8125
 
-# Image capture dimensions
-# Max res: 3280 x 2464
-# MAX WORKING DIMENSIONS
-WIDTH = 3264
-HEIGHT = 2464
-
 # Load camera properties matrices from file
 # This file is generated from the camera calibration
-KD = np.load('CV_CameraCalibrationMatrices.npz')
+KD = np.load('CV_CameraCalibrationMatrices_'+width+'x'+height+'.npz')
 K = KD['k']
 DIST_COEFFS = KD['dist']
 
@@ -147,8 +125,6 @@ def detect_marker(img):
     # If an Aruco marker is detected
     if ids is not None:
         print("Marker detected")
-##        lcd.clear()
-##        lcd.message = "Marker detected"
         for tag in ids:
             cv.aruco.drawDetectedMarkers(image=img,
                                          corners=corners,
@@ -161,10 +137,8 @@ def detect_marker(img):
     # If an Aruco marker is not detected
     if ids is None:
         print("Marker not detected")
-##        lcd.clear()
-##        lcd.message = "Marker not detected"
         # Return zeros
-        angle_deg = 360
+        angle_deg = 180
         angle_rad = angle_deg * math.pi / 180
         distance = 0
 
@@ -181,7 +155,7 @@ def get_vals(corners):
         )
 
     # Unpack translation vector
-    # This vector contains x, y, z distances of tag from camera
+    # This vector contains x, y, z distances of marker from camera
     t_vec = tvecs[0][0]
 
     # Calculate distance using the root of the sum of the squares
@@ -202,10 +176,6 @@ def get_vals(corners):
 
 def state0(state):
     print("Running State 0")
-##    width = int(WIDTH * SMALL_SCALE)
-##    height = int(HEIGHT * SMALL_SCALE)
-##    camera.resolution = (width, height)
-##    camera.exposure_mode = 'sports'
 
     vid = cv.VideoCapture(0)
 
@@ -214,9 +184,6 @@ def state0(state):
 
         # Convert to grayscale for Aruco detection
         gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-                
-        # Detect Aruco marker, and get detected angle and distance
-##          lcd.message = '\n' + str(round(angle_deg, 2)) + " degrees"
 
         # Detect if Aruco marker is present
         corners, ids, _ = cv.aruco.detectMarkers(image=img,
@@ -255,8 +222,8 @@ def state0(state):
 
 def state1():
     print("Running State 1")
-    camera = PiCamera()
-    camera.resolution = (WIDTH, HEIGHT)
+    
+    camera = PiCamera(resolution=(WIDTH, HEIGHT))
 
     # Set up picamera array
     with picamera.array.PiRGBArray(camera) as stream:
@@ -269,7 +236,6 @@ def state1():
                 
         # Detect Aruco marker, and get detected angle and distance
         distance, angle_deg, angle_rad, img = detect_marker(gray_img)
-##            lcd.message = '\n' + str(round(angle_deg, 2)) + " degrees"
 
         # Optional display stream images
         if DISP_IMG is True:
@@ -277,34 +243,37 @@ def state1():
             disp_img = disp_resize(img)
             # Display image
             cv.imshow("Precise Img", disp_img)
-            cv.waitKey(0)
+            cv.waitKey(STATE1_WAITKEY)
             cv.destroyWindow("Precise Img")
                 
         # Truncate the stream to clear for next image capture
         stream.truncate(0)
+
+    return distance, angle_deg, angle_rad
   
             
 if __name__ == '__main__':
     # Initialize state
     state = 0
-    start = time()
 
     while True:
         if state == 0:
+            # State 0 for continuous video search for Aruco marker
             state = state0(state)
-            # BROKEN
-            if TIMING:
-                stop = time()
-                time = stop - start
-                print("--- %s seconds ---" % round(time, 3))
-                fps = 1 / time
-                fps = str(round(fps, 3))
-                print("--- %s fps ---" % fps)
                 
         if state == 1:
-            state1()
+            # Get state1 start time
+            start_time = time()
+            # State 1 for still image capture to localize Aruco marker
+            distance, angle_deg, angle_rad = state1()
+
+            # Print the runtime of the program
+            print("state1 runtime: ", time() - start_time, "seconds")
+    
             # End after state 1
             break;
-        
+
+    
+     
     
             
